7116b58950aef10cf367d402cb61c4cb539bb408
Dori C <itsdori@gmail.com>
2022-03-23 08:42:30 -0700

    CR de CRUD done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,14 +10,19 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
-  
+
   def create
-    @article = Article.new(title: "..", body: "..")
+    @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
-    else 
+    else
       render :new, status: :unprocessable_entity
     end
   end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
